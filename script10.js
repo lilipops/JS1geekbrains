c06bc5be7d255bb272ecd70d3681976d88ed311b
Nikita Gurskiy <85452618+lilipops@users.noreply.github.com>
2022-08-30 15:59:52 +0200

    Add files via upload

---
+++ script10.js
@@ -0,0 +1,7 @@
+// LESSON 3 EXERCISE 4
+let cruzes = 'x'
+for (let i = 0; i < 20; i++) {
+    console.log(cruzes)
+    cruzes += 'x'
+    
+}
---
+++ script9.js
@@ -0,0 +1,5 @@
+//lesson 3 exercise number 3
+for (let i = 0;i  <= 9;console.log(i++)) {
+    
+    
+}
